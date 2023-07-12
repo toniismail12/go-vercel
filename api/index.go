67695d6d6e7ba4fe3eecbad3db60ee5f41bca0e4
toniismailz <tonismail20@gmail.com>
2023-07-12 10:26:41 +0700

    first push"

---
+++ api/index.go
@@ -0,0 +1,10 @@
+package handler
+
+import (
+  "fmt"
+  "net/http"
+)
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Hello World from Go!")
+}
+
---
+++ go.mod
@@ -0,0 +1,3 @@
+module go-vercel
+
+go 1.19
